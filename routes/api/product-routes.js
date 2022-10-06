fcce7988268581132d56b3de907b68eca7ddde66
Carl Bergquist <carlbergquist81@gmail.com>
2022-10-06 00:36:12 -0500

    read me and tag routes complete--final

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   const deletedProduct = await Product.destroy({
     where: {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,16 +38,30 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  const createTag = await Tag.create(req.body);
+  res.status(200).json(createTag)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const updatedTag = await Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  res.status(200).json(updatedTag)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const deletedTag = await Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json(deletedTag)
 });
 
 module.exports = router;
