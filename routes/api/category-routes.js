aa6709df98222500ecf88ed2c1be0e1785398f4f
Carl Bergquist <carlbergquist81@gmail.com>
2022-10-06 00:17:19 -0500

    cat routes done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,16 +37,30 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const createCat = await Category.create(req.body);
+  res.status(200).json(createCat)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const updatedCat = await Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  res.status(200).json(updatedCat)
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const deletedCat = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json(deletedCat)
 });
 
 module.exports = router;
